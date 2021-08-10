3cc226ea7835574e8c5339ba49e0a14242af68de
smooth3747 <balla2005@icloud.com>
2021-08-10 11:55:52 -0400

    update

--- index.js
+++ index.js
@@ -1,12 +1,78 @@
-const express = require('express')
-const app = express()
-const port = 8000
+const express = require('express');
+const app = express();
+const faker = require('faker');
+const _ = require('lodash');
+const port = 8000;
+const fakerReminders = faker.git.commitMessage();
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-app.post('/create', (req, res) => {
-  res.send('create reminder')
-})
+  const count = 1;
+  if (!count) {
+    console.log('in 400 error');
+    return res
+      .status(400)
+      .send({ errorMsg: 'count query parameter is missing' });
+  } else if (fakerReminders) {
+    console.log('in 200 status');
+    const uuid = faker.datatype.uuid();
+    const reminderObject = {
+      id: uuid,
+      reminder: fakerReminders,
+    };
+    console.log(reminderObject);
+    res.status(200).json(reminderObject);
+  } else {
+    console.log('in 500 error');
+    err => {
+      console.log('error', err);
+      res
+        .status(500)
+        .json({
+          message: 'error, server has crashed running get all reminders',
+        });
+    };
+  }
+});
+app.get('/', (req, res) => {
+  const count = 1;
+  if (!count) {
+    console.log('in 400 error');
+    return res
+      .status(400)
+      .send({ errorMsg: 'count query parameter is missing' });
+  } else if (fakerReminders) {
+    console.log('in 200 status');
+    const uuid = faker.datatype.uuid();
+    const reminderObject = {
+      id: uuid,
+      reminder: fakerReminders,
+    };
+    console.log(reminderObject);
+    res.status(200).json(reminderObject);
+  } else {
+    console.log('in 500 error');
+    err => {
+      console.log('error', err);
+      res
+        .status(500)
+        .json({
+          message: 'error, server has crashed running get all reminders',
+        });
+    };
+  }
+});
+app.get('', (req, res) => {
+  todos
+    .get()
+    .then(todos => {
+      if (todos) {
+        res.status(200).json(todos);
+      }
+    })
+    .catch(err => {
+      console.log('err', err);
+      res.status(500).json({ message: 'Server Error' });
+    });
+});
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+  console.log(`====Example app listening at http://localhost:${port}=====`);
+});
---
+++ knexFile.js
@@ -0,0 +1,36 @@
+const localPgConnection = {
+    host: "localhost",
+    database: "reminder",
+    user: "vance",
+    password: process.env.DATABASE_PASSWORD
+  };
+  const dbConnection = process.env.DATABASE_URL || localPgConnection;
+  module.exports = {
+    development: {
+      client: "sqlite3",
+      connection: {
+        filename: "./db/dev.sqlite3"
+      },
+      useNullAsDefault: true,
+      migrations: {
+        directory: "./db/migrations"
+      },
+      seeds: {
+        directory: "./db/seeds"
+      }
+    },
+    production: {
+      client: "pg",
+      connection: dbConnection,
+      pool: {
+        min: 2,
+        max: 10
+      },
+      migrations: {
+        directory: "./db/migrations"
+      },
+      seeds: {
+        directory: "./db/seeds"
+      }
+    }
+  };
